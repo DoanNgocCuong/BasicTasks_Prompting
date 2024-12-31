b23df2d8727d02fd20b37e4958c4fe0ae96d71a0
Doan Ngoc Cuong <cuongmkmtpgoldfinch@gmail.com>
2024-12-31 19:10:20 +0700

    update để cố định hơn response với các tham số khác ngoài temperature=0 +++++ Consume 30 min cho phần check response này -)))
    temperature=0,
      max_completion_tokens=2048,
      top_p=1,
      frequency_penalty=0,
      presence_penalty=0

--- 6_TuningWith2Prompting/def_promptA_v1.py
+++ 6_TuningWith2Prompting/def_promptA_v1.py
@@ -21,6 +21,10 @@ def generate_roleA_response(client, roleA_prompt, message_history):
         model="gpt-4o-mini",
         messages=api_messages,
         temperature=0,
+        max_completion_tokens=2048,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
     )
     end_time = time.time()
     
